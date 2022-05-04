0f1f5f0aa017a41503efa7efc8d43b5df2f39afb
ochanman <david.godignon@lapiscine.pro>
2022-05-04 11:03:04 +0200

    4 graphs

--- app.py
+++ app.py
@@ -86,7 +86,7 @@ def page_2_radios(value):
 
 
 
-
+# --------------- filtrage 1er graph ---------------------
 
 # selection des colonnes
 overview_fr_selec = df.loc[:, ["tconst", "primaryTitle", "averageRating", "director1", "budget", "revenue"]]
@@ -102,15 +102,71 @@ overview_fr_by_budget = overview_fr_by_budget.nlargest(20, 'budget')
 
 
 
-fig = px.bar(overview_fr_by_budget, x='director1', y='budget',
+fig_overview_fr_by_budget = px.bar(overview_fr_by_budget, x='director1', y='budget',
     title="Les 20 realisateurs ayant eu le plus de budget",
     labels={
                      "budget": "Budget en $",
                      "director1": "Realisateurs"
                  },
-    color='budget')
+    color='budget', width=600, height=400)
 
+# --------------- filtrage 2eme graph ---------------------
 
+# je créé un dataframe avec 20 realisateurs ayant fait le plus de films
+director_count = overview_fr_selec.value_counts(['director1']) 
+director_count = pd.DataFrame(director_count, columns = ["Quantité"])
+director_count_top20 = director_count.nlargest(20, 'Quantité')
+director_count_top20 = director_count_top20.rename_axis("Director").reset_index()
+
+# Les 20 realisateurs qui réalisé le plus films
+fig_director_count_top20 = px.bar(director_count_top20, x='Director', y='Quantité',
+    title="Les 20 realisateurs qui réalisé le plus films",
+    labels={
+                     "Quantité": "Quantité",
+                     "Director": "Realisateurs"
+                 },
+    color='Quantité', width=600, height=400)
+
+# --------------- filtrage 3eme graph ---------------------
+
+# sort by revenue
+overview_fr_by_revenue = overview_fr_selec.sort_values(by=["revenue"], ascending=False, inplace=False)
+# réalisateurs qui ont rapporté le plus d’argent 
+overview_fr_by_revenue = overview_fr_by_revenue.groupby('director1')['revenue'].sum().reset_index()
+overview_fr_by_revenue.sort_values(by=["revenue"], ascending=False, inplace=True)
+overview_fr_by_revenue = overview_fr_by_revenue.nlargest(20, 'revenue')
+
+# Les 20 réalisateurs qui ont rapporté le plus d’argent 
+fig_overview_fr_by_revenue = px.bar(overview_fr_by_revenue, x='director1', y='revenue',
+    title="Les 20 réalisateurs qui ont rapporté le plus d’argent ",
+    labels={
+                     "revenue": "revenue en $",
+                     "director1": "Realisateurs"
+                 },
+    color='revenue', width=600, height=400)
+
+# --------------- filtrage 4eme graph ---------------------
+
+# selection des colonnes
+overview_fr_selec_with_time = df.loc[:, ["tconst", "primaryTitle", "startYear", "director1", "budget", "revenue"]]
+# drop NaN
+overview_fr_selec_with_time.dropna(inplace=True)
+# sort by revenue
+overview_fr_selec_with_time = overview_fr_selec_with_time.sort_values(by=["revenue"], ascending=False, inplace=False)
+# je supprime tous les films qui n'ont pas de revenues
+overview_fr_selec_with_time=overview_fr_selec_with_time[overview_fr_selec_with_time["revenue"] > 0]
+# sort by Years
+overview_fr_selec_with_time = overview_fr_selec_with_time.sort_values(by=["startYear"], ascending=True, inplace=False)
+
+# Les réalisateurs qui ont rapporté le plus d’argent par année 
+fig_overview_fr_selec_with_time = px.bar(overview_fr_selec_with_time, x='director1', y='revenue',
+    title="Les réalisateurs qui ont rapporté le plus d’argent par année ",
+    labels={
+                     "revenue": "revenue en $",
+                     "director1": "Realisateurs"
+                 },
+    color='revenue', 
+                    animation_frame ="startYear")
 
 page_3_layout = html.Div([
     html.H1('Réalisateurs'),
@@ -123,8 +179,21 @@ page_3_layout = html.Div([
     dbc.Button('Recommandations', outline=True, color="primary", href='/', className='btn'),
     dcc.Graph(
         id='example-graph',
-        figure=fig
+        figure=fig_overview_fr_by_budget
     ),
+    dcc.Graph(
+        id='example-graph',
+        figure=fig_director_count_top20
+    ),
+    dcc.Graph(
+        id='example-graph',
+        figure=fig_overview_fr_by_revenue
+    ),
+    dcc.Graph(
+        id='example-graph',
+        figure=fig_overview_fr_selec_with_time
+    ),
+
 ]),
 
 

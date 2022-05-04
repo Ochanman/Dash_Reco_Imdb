b8e358fd393f9c9143981ab998b67538fe173ce8
ochanman <david.godignon@lapiscine.pro>
2022-05-04 10:28:11 +0200

    1er graph

--- app.py
+++ app.py
@@ -2,8 +2,23 @@ from dash import Dash, html, dcc, Input, Output
 import numpy as np
 import pandas as pd
 import plotly.graph_objects as go
+import plotly.express as px
 import dash_bootstrap_components as dbc
 import dash_dangerously_set_inner_html
+
+# Data import
+df = pd.read_csv("assets/overview_fr.csv")
+
+
+
+
+# df = pd.DataFrame({
+#     "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+#     "Amount": [4, 1, 2, 2, 4, 5],
+#     "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+# })
+
+
 
 # Initialisation de l'app
 app = Dash(__name__, suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.BOOTSTRAP])
@@ -70,6 +85,33 @@ def page_2_radios(value):
     return f'You have selected {value}'
 
 
+
+
+
+# selection des colonnes
+overview_fr_selec = df.loc[:, ["tconst", "primaryTitle", "averageRating", "director1", "budget", "revenue"]]
+# drop NaN
+overview_fr_selec.dropna(inplace=True)
+# sort by budget
+overview_fr_by_budget = overview_fr_selec.sort_values(by=["budget"], ascending=False, inplace=False)
+# je créé un dataframe avec 20 realisateurs ayant eu le plus de budget
+
+overview_fr_by_budget = overview_fr_by_budget.groupby('director1')['budget'].sum().reset_index()
+overview_fr_by_budget.sort_values(by=["budget"], ascending=False, inplace=True)
+overview_fr_by_budget = overview_fr_by_budget.nlargest(20, 'budget')
+
+
+
+fig = px.bar(overview_fr_by_budget, x='director1', y='budget',
+    title="Les 20 realisateurs ayant eu le plus de budget",
+    labels={
+                     "budget": "Budget en $",
+                     "director1": "Realisateurs"
+                 },
+    color='budget')
+
+
+
 page_3_layout = html.Div([
     html.H1('Réalisateurs'),
     html.Div(id='page-3-content'),
@@ -78,13 +120,18 @@ page_3_layout = html.Div([
     dbc.Button('Réalisateurs', outline=True, color="primary", href='/page-3', className='btn'),
     dbc.Button('Genres', outline=True, color="primary", href='/page-4', className='btn'),
     dbc.Button('Durée', outline=True, color="primary", href='/page-5', className='btn'),
-    dbc.Button('Recommandations', outline=True, color="primary", href='/', className='btn')
-])
+    dbc.Button('Recommandations', outline=True, color="primary", href='/', className='btn'),
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    ),
+]),
 
-@app.callback(Output('page-3-content', 'children'),
-              [Input('page-3-radios', 'value')])
-def page_3_radios(value):
-    return f'You have selected {value}'
+
+
+
+
+
 
 
 page_4_layout = html.Div([
